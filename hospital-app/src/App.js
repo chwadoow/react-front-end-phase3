b4f1c2e7abbd9d6bb088da94652b8dafe642ffda
moses ngige <moses32ngigz@gmail.com>
2022-09-09 03:21:39 +0300

    routes added

--- hospital-app/src/App.js
+++ hospital-app/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './style.css';
+import    React from "react"
+import {Switch,Route} from 'react-router-dom'
+import Home from './components/Home';
+import CreateAppointment from './components/CreateAppointment';
+import NavBar from './components/NavBar';
+import AppointmentDetail from './components/Appointment'
+import Appointments from './components/Appointments';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <NavBar/ >
+      <Switch>
+
+    <Route  path="/appointments">
+    <Appointments/>
+    </Route>
+
+    <Route  path="/appointments/:id">
+    <AppointmentDetail/>
+    </Route>
+
+    <Route  path='/createappointments'>
+    <CreateAppointment/>
+    </Route>
+
+ 
+    <Route  exact path='/'>
+    <Home/>
+      </Route >
+  
+      <Route path='*'>
+      <h1>404 not found</h1>
+      </Route>
+
+
+
+      </Switch>
+      
+    
+ 
     </div>
   );
 }
