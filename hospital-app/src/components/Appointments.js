f3b58a96742bf40d3f7436435775b5cc7467aace
moses ngige <moses32ngigz@gmail.com>
2022-09-09 03:25:08 +0300

    added fetch function to fetch appointments

---
+++ hospital-app/src/components/Appointments.js
@@ -0,0 +1,41 @@
+import React , {useEffect,useState}from 'react'
+import AppointmentList from './AppointmentList';
+import CreateAppointment from './CreateAppointment';
+function Appointments() {
+
+    const [appointments,setAppointments]= useState([])
+    useEffect(() => {
+        fetch("http://localhost:9292/appointments")
+          .then((r) => r.json())
+          .then((data) => setAppointments(data));
+      }, []);
+     
+    function handleDeleteItem (id){
+        const updatedItems = appointments.filter((item)=> item.id !== id)
+        setAppointments(updatedItems)
+      }    
+
+      function handleAddAppointment(newAppointment){
+        setAppointments([...appointments,newAppointment])
+      }
+  return (
+  
+            <div >
+                <CreateAppointment
+                onAddAppointment={handleAddAppointment}
+                />
+      
+      <h3>Appointments</h3>
+
+  <AppointmentList
+  appointments={appointments}
+  //funcs
+  onItemDelete={handleDeleteItem}
+  />   
+
+
+        </div>
+  )
+}
+
+export default Appointments
