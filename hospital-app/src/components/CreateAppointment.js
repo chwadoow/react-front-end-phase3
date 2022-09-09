6fbf3a62d29d5dfb93dd88160aa74331ceea9b56
moses ngige <moses32ngigz@gmail.com>
2022-09-09 03:27:44 +0300

    still working on form to add appointments

---
+++ hospital-app/src/components/CreateAppointment.js
@@ -0,0 +1,83 @@
+ import React,{useState} from 'react'
+ 
+ function CreateAppointment({handleAddAppointment}) {
+    const[formData, setFormData] = useState({
+        appointment_duration: '',
+        appointment_reason: '',
+        type: '',
+        doc_id: '',
+        patient_id: '',
+       
+    })
+
+
+    function handleChange(e){
+
+   
+        setFormData({...formData, [e.target.id]: e.target.value});
+    }
+
+    function handleSubmit(e){
+        e.preventDefault()
+        fetch(`http://localhost:9292/appointments`, {
+            method: "POST",
+            headers: {
+                "Content-Type" : "application/json"
+            },
+            body: JSON.stringify(formData),
+        })
+        .then((r) => r.json())
+        .then((data) => handleAddAppointment(data))
+        setFormData(
+            {
+                appointment_duration: '',
+                appointment_reason: '',
+                type: '',
+                doc_id: '',
+                patient_id: '',
+               
+            }
+        )
+    }
+
+
+
+   return (
+     <div>
+         <form className="row g-3" onSubmit={handleSubmit}>
+         <div className="col-md-6">
+                    <label className="form-label">Enter Datetime</label>
+                    <input type="datetime-local" className="form-control" id="appointment_date"  placeholder="Enter Duration"  onChange={handleChange} value={formData.appointment_date} required/>
+                </div>
+                  <div className="col-md-6">
+                    <label className="form-label">Duration</label>
+                    <input type="integer" className="form-control" id="appointment_duration"  placeholder="Enter Duration"  onChange={handleChange} value={formData.appointment_duration} required/>
+                </div>
+                <div className="col-md-6">
+                    <label className="form-label">Reason</label>
+                    <input type="text" className="form-control" id="appointment_reason" onChange={handleChange} placeholder="Enter Reason" value={formData.appointment_reason} required/>
+                </div>
+                <div className="col-md-6">
+                    <label className="form-label">Type</label>
+                    <input type="text" className="form-control" id="type" placeholder="Type" onChange={handleChange} value={formData.type} required/>
+                </div>
+                <div className="col-md-6">
+                    <label className="form-label">Doc_id</label>
+                    <input type="integer" className="form-control" id="doc_id" placeholder="Enter doc_id" onChange={handleChange} value={formData.doc_id} required/>
+                </div>
+                <div className="col-md-6">
+                    <label className="form-label">Patient_id</label>
+                    <input type="integer" className="form-control" id="patient_id" onChange={handleChange} value={formData.patient_id} required/>
+                </div>
+                <button type="submit" className="btn btn-primary">Create</button>
+                </form>
+        
+
+
+
+
+     </div>
+   )
+ }
+ 
+ export default CreateAppointment
